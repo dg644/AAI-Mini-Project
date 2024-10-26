14d1bf9eec1d8b3f4ddae5adaa62ed002588c4e8
Dylan Green <dg644@cam.ac.uk>
2024-10-26 10:46:59 +0100

    Split data by person to test fairness, and into train/val/test.

--- CNN-classifier/src/preprocess.py
+++ CNN-classifier/src/preprocess.py
@@ -2,6 +2,7 @@ import os
 import glob
 import shutil
 import random
+from tqdm import tqdm
 
 # PSPI 0 = no pain, 1 = trace, 2 = weak, >=3 = strong
 # just doing no pain and pain so 0 = no pain, >=1 = pain
@@ -22,7 +23,7 @@ def label_images(label_dir, dataset_dir):
     label_paths = glob.glob(os.path.join(dataset_dir, label_dir, '*/*/*.txt'))
     
     labels = {}
-    for label_path in label_paths:
+    for label_path in tqdm(label_paths, desc="Labeling images"):
         with open(label_path, 'r') as file:
             for line in file:
                 pspi = line.strip().split(',')[0]
@@ -34,17 +35,17 @@ def label_images(label_dir, dataset_dir):
                 else:
                     labels[filename] = 0 # no_pain
     
-    print("Labels complete!")
+    print("Labels complete!\n")
     return labels
 
 
 
-# sort images into pain and no pain folders and then into train and val folders
+# sort images into pain and no pain folders and then into train and test folders
 def sort_images(image_dir, label_dir, project_dir, dataset_dir):
     labels = label_images(label_dir, dataset_dir)
     image_paths = glob.glob(os.path.join(dataset_dir, image_dir, '*/*/*.png'))
     
-    for image_path in image_paths:
+    for image_path in tqdm(image_paths, desc="Sorting images"):
         filename = os.path.basename(image_path)
         label = labels.get(filename)
         if label:
@@ -56,11 +57,33 @@ def sort_images(image_dir, label_dir, project_dir, dataset_dir):
         if not os.path.exists(dest_path):
             shutil.copy(image_path, dest_path)
     
-    print("Sort complete!")
+    print("Sort complete!\n")
 
+def split_by_person(image_dir):
+    person_images = {}
+    image_list = os.listdir(image_dir)
+    for image in tqdm(image_list, desc="Processing images by person"):
+        person_id = image.split('t')[0]  # assuming person id is before 't' in filename
+        if person_id not in person_images:
+            person_images[person_id] = []
+        person_images[person_id].append(image)
+    
+    train_images = []
+    val_images = []
+    test_images = []
+    random.seed = 42
+    for person_id, images in person_images.items():
+        random.shuffle(images)
+        train_split_idx = int(0.6 * len(images))
+        val_split_idx = int(0.8 * len(images))
+        train_images.extend(images[:train_split_idx])
+        val_images.extend(images[train_split_idx:val_split_idx])
+        test_images.extend(images[val_split_idx:])
+    
+    return train_images, val_images, test_images
 
-# splut images into train and val folders
-def split_images(project_dir, train_dir, val_dir):
+# split images into train and test folders
+def split_images(project_dir, train_dir, val_dir, test_dir):
     pain_dir = os.path.join(project_dir, 'sorted-images', 'pain')
     no_pain_dir = os.path.join(project_dir, 'sorted-images', 'no-pain')
     
@@ -68,50 +91,51 @@ def split_images(project_dir, train_dir, val_dir):
     train_no_pain_dir = os.path.join(project_dir, train_dir, 'no-pain')
     val_pain_dir = os.path.join(project_dir, val_dir, 'pain')
     val_no_pain_dir = os.path.join(project_dir, val_dir, 'no-pain')
+    test_pain_dir = os.path.join(project_dir, test_dir, 'pain')
+    test_no_pain_dir = os.path.join(project_dir, test_dir, 'no-pain')
     
     os.makedirs(train_pain_dir, exist_ok=True)
     os.makedirs(train_no_pain_dir, exist_ok=True)
     os.makedirs(val_pain_dir, exist_ok=True)
     os.makedirs(val_no_pain_dir, exist_ok=True)
+    os.makedirs(test_pain_dir, exist_ok=True)
+    os.makedirs(test_no_pain_dir, exist_ok=True)
 
     # ensure the directories are empty
-    for dir_path in [train_pain_dir, train_no_pain_dir, val_pain_dir, val_no_pain_dir]:
+    for dir_path in [train_pain_dir, train_no_pain_dir, val_pain_dir, val_no_pain_dir, test_pain_dir, test_no_pain_dir]:
         for file in os.listdir(dir_path):
             file_path = os.path.join(dir_path, file)
             if os.path.isfile(file_path):
                 os.remove(file_path)
 
-    pain_images = os.listdir(pain_dir)
-    no_pain_images = os.listdir(no_pain_dir)
+    train_pain_images, val_pain_images, test_pain_images = split_by_person(pain_dir)
+    train_no_pain_images, val_no_pain_images, test_no_pain_images = split_by_person(no_pain_dir)
     
-    random.shuffle(pain_images)
-    random.shuffle(no_pain_images)
-    
-    train_pain_images = pain_images[:int(0.8*len(pain_images))]
-    val_pain_images = pain_images[int(0.8*len(pain_images)):]
-    train_no_pain_images = no_pain_images[:int(0.8*len(no_pain_images))]
-    val_no_pain_images = no_pain_images[int(0.8*len(no_pain_images)):]
-    
-    for image in train_pain_images:
+    for image in tqdm(train_pain_images, desc="Copying train pain images"):
         shutil.copy(os.path.join(pain_dir, image), os.path.join(train_pain_dir, image))
-    for image in val_pain_images:
+    for image in tqdm(val_pain_images, desc="Copying val pain images"):
         shutil.copy(os.path.join(pain_dir, image), os.path.join(val_pain_dir, image))
-    for image in train_no_pain_images:
+    for image in tqdm(test_pain_images, desc="Copying test pain images"):
+        shutil.copy(os.path.join(pain_dir, image), os.path.join(test_pain_dir, image))
+    for image in tqdm(train_no_pain_images, desc="Copying train no pain images"):
         shutil.copy(os.path.join(no_pain_dir, image), os.path.join(train_no_pain_dir, image))
-    for image in val_no_pain_images:
+    for image in tqdm(val_no_pain_images, desc="Copying val no pain images"):
         shutil.copy(os.path.join(no_pain_dir, image), os.path.join(val_no_pain_dir, image))
+    for image in tqdm(test_no_pain_images, desc="Copying test no pain images"):
+        shutil.copy(os.path.join(no_pain_dir, image), os.path.join(test_no_pain_dir, image))
     
-    print("Split complete!")
+    print("Split complete!\n")
 
 
 
 def main():
-    dataset_dir = r'C:\Users\dylan\OneDrive - University of Cambridge\Part II\Affective AI\Project'
-    project_dir = r'C:\Users\dylan\work\AAI-Mini-Project\CNN-classifier'
+    dataset_dir = r'C:\Users\dylan\OneDrive - University of Cambridge\Part II\Affective AI\Project' # change this to the path of the dataset
+    project_dir = r'C:\Users\dylan\work\AAI-Mini-Project\CNN-classifier' # change this to the path of the project
     image_dir = 'UNBC/Images'
     label_dir = 'UNBC/Frame_Labels/PSPI'
     train_dir = 'data/train'
     val_dir = 'data/val'
+    test_dir = 'data/test'
 
     # Example usage of label_images
     # labels = label_images(label_dir, dataset_dir)
@@ -119,10 +143,10 @@ def main():
     # print(labels['ll042t1aaaff022.png']) # 1 = pain (PSPI = 2)
 
     # uncomment to run sort_images
-    # sort_images(image_dir, label_dir, project_dir, dataset_dir)
+    sort_images(image_dir, label_dir, project_dir, dataset_dir)
 
     # uncomment to run split_images
-    split_images(project_dir, train_dir, val_dir)
+    split_images(project_dir, train_dir, val_dir, test_dir)
 
 if __name__ == '__main__':
     main()
